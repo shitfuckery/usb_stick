5943ffbe9970d05f41500b8539e001be5d907fb7
Shitfuckery.ca <code@shitfuckery.ca>
2026-01-19 22:35:54 -0800

    Add wireguard-tools and systemd-resolved

--- setup.sh
+++ setup.sh
@@ -423,7 +423,7 @@ if [ ! -f ~/$SETUP_PROGRESS/$SETUP_INSTALLNETSOFT ]; then
 		echo "deb [arch=amd64 signed-by=/usr/share/keyrings/signal-desktop-keyring.gpg] https://updates.signal.org/desktop/apt xenial main" |\
 			sudo tee /etc/apt/sources.list.d/signal-xenial.list
 		apt-get update
-		apt-get install -y signal-desktop torbrowser-launcher chromium
+		apt-get install -y signal-desktop torbrowser-launcher chromium wireguard-tools systemd-resolved
 
 		touch ~/$SETUP_PROGRESS/$SETUP_INSTALLNETSOFT
 
